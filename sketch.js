9c6e7c1a0539a5538eaff033876feff1e44b45e1
CnnJu <jucanon90@gmail.com>
2024-11-04 11:35:34 +0100

    test size canvas and mouse following circle

--- sketch.js
+++ sketch.js
@@ -3,11 +3,11 @@ function setup() {
 }
 
 function draw() {
-  background(220,5,16);
+  background(16);
   if (mouseIsPressed === true) {
     fill(0);
   } else {
-    fill(10, 60, 255);
+    fill(255);
   }
 
 
