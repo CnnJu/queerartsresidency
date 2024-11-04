a5c86f08895b6f20d2da43f8b65e95eb3ce3ecee
CnnJu <jucanon90@gmail.com>
2024-11-04 11:34:25 +0100

    test

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
 }
 
 function draw() {
-  background(220);
+  background(220,5,16);
+  if (mouseIsPressed === true) {
+    fill(0);
+  } else {
+    fill(10, 60, 255);
+  }
+
+
+  circle(mouseX, mouseY, 100);
+}
 }
